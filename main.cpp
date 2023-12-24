6d67b3fab2cf56839195f4947b654215d523677a
Anas Elhaag <86550384+at2nothing@users.noreply.github.com>
2023-12-24 21:46:08 +0200

    Delete unnecessary files and refactor code

--- main.cpp
+++ main.cpp
@@ -12,9 +12,12 @@ void new_account();
 int account_num_creation();
 void print(string text);
 void format_on();
+void format_out_on();
 void format_off();
 int check_int_input(int min, int max);
 int check_passcode();
+void print_error(string text) ;
+
 
 // Account structure
 struct Account {
@@ -42,12 +45,7 @@ string type;
 string cancel;
 
 // Main function for the program. Prints a welcome message and prompts the user for a choice
-/**
-* @brief Main function of the program. Prints welcome message and prompts user for choice. Calls new_account () or ATM () depending on user choice.
-* @return Exit code ( 0 means success non - zero means failure ). In case of error the program exits
-*/
 int main() {
-    // This method is called by the user to ask for a new bank account or ATM
     while (true) {
         print(
             "Welcome to our bank\n"
@@ -58,10 +56,8 @@ int main() {
         choice = check_int_input(1, 2);
 
         // User choices
-        // if choice 1 new_account new_account ATM
         if (choice == 1) {
             new_account();
-        // If choice is 2 ATM is the choice 2
         } else if (choice == 2) {
             ATM();
         }
@@ -70,18 +66,11 @@ int main() {
 }
 
 // Account number creation function
-/**
-* @brief Returns the number of accounts that have been created. This is used to determine how many accounts will be created in the case of an error or when a user tries to create more than one account at a time.
-* @return The number of accounts that have been created or - 1 if there are no accounts in the account vector
-*/
 int account_num_creation() {
     return accounts_vector.size() + 1;
 }
 
 // New account creation function
-/**
-* @brief Create a new account in the database and add it to the accounts_vector. This is a function that should be called every time the user enters an account
-*/
 void new_account() {
     Account account;
     print(
@@ -93,7 +82,7 @@ void new_account() {
     account.password = check_passcode();
     cout << endl;
     print("Enter your balance: ");
-    account.balance = check_int_input(0, 1000000000);
+    account.balance = check_int_input(0, 100000000);
     cout << endl;
 
     // Set account type to GOLD or SILVER
@@ -103,18 +92,21 @@ void new_account() {
     accounts_vector.push_back(account);
 
     format_on();
-    cout << "Your account is created successfully\n";
-    cout << "Your account number is " << account.acc_number << endl;
-    cout << "Your account type is " << account.type << endl;
-    cout << "Your balance is $" << account.balance << endl;
+    print("Your account is created successfully\n");
+    print ( "Your account number is ") ;
+    format_out_on();
+    cout<< account.acc_number << endl;
+    print ("Your account type is ");
+    format_out_on();
+    cout<< account.type << endl;
+    print("Your balance is $");
+    format_out_on();
+    cout << account.balance << endl;
     cout << endl;
     format_off();
 }
 
 // ATM function
-/**
-* @brief \ brief ATM for this program \ author Sascha Kevin Olivier Cou
-*/
 void ATM() {
     print(
         "Welcome to the ATM\n"
@@ -130,32 +122,29 @@ void ATM() {
             return accountNumber == account.acc_number;
         });
 
-    // Perform a transaction on the account iterator.
     if (accountIterator == accounts_vector.end()) {
-        print(
+        print_error(
             "Invalid account number\n"
             "Please try again later\n\n\n");
     } else {
         int attempts = 3;
-        // Perform a transaction on the account iterator.
         while (attempts > 0) {
             print("Please enter your PIN number: ");
             int enteredPin = check_int_input(0, 10000);
             cout << endl;
 
-            // Perform a transaction if the user entered the password.
             if (enteredPin == accountIterator->password) {
                 performTransaction(*accountIterator);
                 break;
             } else {
-                format_on();
-                cout << "Incorrect PIN. You have " << attempts - 1 << " attempts left.\n";
+                print( "Incorrect PIN. You have " );
+                format_out_on();
+                cout<< attempts - 1 ;
+                print(" attempts left.\n") ;
                 attempts--;
-                format_off();
             }
         }
 
-        // Prints a message if there are no more attempts.
         if (attempts == 0) {
             print("Too many incorrect attempts. Exiting...\n");
         }
@@ -163,15 +152,10 @@ void ATM() {
 }
 
 // Additional functions for transaction handling
-/**
-* @brief Performs the transaction and prompts the user to choose. This is a blocking function. It returns when the user confirms or exits
-* @param account Reference to the account
-*/
 void performTransaction(Account &account) {
     print("Login successful!\n");
     printAccountInfo(account);
 
-    // Choose the action 1 Cash Withdrawal 2 Deposit 3 Fund Transfer 4 Exit
     while (true) {
         print(
             "Choose the action:\n"
@@ -184,7 +168,6 @@ void performTransaction(Account &account) {
         int choice = check_int_input(1, 4);
         cout << endl;
 
-        // Perform the action based on the choice.
         switch (choice) {
             case 1:
                 performWithdrawal(account);
@@ -199,35 +182,28 @@ void performTransaction(Account &account) {
                 print("Exiting...\n");
                 return;
             default:
-                print("Invalid choice. Please try again.\n");
+                print_error("Invalid choice. Please try again.\n");
         }
     }
 }
 
-/**
-* @brief Withdraws funds from the account. This function is called by the program when the user presses the withdraw button.
-* @param account Reference to the account to withdraw from. This is passed by reference
-*/
 void performWithdrawal(Account &account) {
     print("Enter the amount to withdraw: ");
     double withdrawalAmount = check_int_input(0, account.balance);
     cout << endl;
 
-    // Withdrawal is successful and the account has been withdrawal.
     if (withdrawalAmount <= account.balance) {
         account.balance -= withdrawalAmount;
         format_on();
-        cout << "Withdrawal successful. Updated balance: $" << account.balance << "\n";
+        print("Withdrawal successful. Updated balance: $" );
+        format_out_on();
+        cout <<  account.balance << "\n";
         format_off();
     } else {
         print("Insufficient funds. Withdrawal canceled.\n");
     }
 }
 
-/**
-* @brief Performs deposit and updates the balance. This is a callback function that will be called by the user when they want to deposit a new amount.
-* @param account Reference to the account being deposited to
-*/
 void performDeposit(Account &account) {
     print("Enter the amount to deposit: ");
     double depositAmount = check_int_input(0, numeric_limits<int>::max());
@@ -235,14 +211,12 @@ void performDeposit(Account &account) {
 
     account.balance += depositAmount;
     format_on();
-    cout << "Deposit successful. Updated balance: $" << account.balance << "\n";
+    print(  "Deposit successful. Updated balance: $");
+    format_out_on();
+    cout << account.balance << "\n";
     format_off();
 }
 
-/**
-* @brief Transfer funds from one account to another. This is a function that does not return until the transfer is complete
-* @param sourceAccount The account to transfer
-*/
 void performFundTransfer(Account &sourceAccount) {
     print("Enter the account number to transfer funds to: ");
     int targetAccountNumber = check_int_input(1, 100);
@@ -255,77 +229,78 @@ void performFundTransfer(Account &sourceAccount) {
             return targetAccountNumber == account.acc_number;
         });
 
-    // Transfer the amount of funds from the source account to the target account.
     if (targetAccountIterator != accounts_vector.end()) {
         print("Enter the amount to transfer: ");
         double transferAmount = check_int_input(0, sourceAccount.balance);
         cout << endl;
 
-        // This method is used to transfer the funds from the source account to the target account.
         if (transferAmount <= sourceAccount.balance) {
             sourceAccount.balance -= transferAmount;
             targetAccountIterator->balance += transferAmount;
 
             print("Fund transfer successful.\n");
             printAccountInfo(sourceAccount);
+            format_on();
+            cout << "Updated balance for target account (#" ;
+            format_out_on();
+            cout<< targetAccountNumber;
+            print("): $")  ;
+            format_out_on();
+            cout<< targetAccountIterator->balance << "\n";
+            format_off();
         } else {
             print("Insufficient funds. Fund transfer canceled.\n");
         }
     } else {
-        print("Invalid target account number. Fund transfer canceled.\n");
+        print_error("Invalid target account number. Fund transfer canceled.\n");
     }
 }
 
 // Print account information
-/**
-* @brief Prints information about an account. This is used to determine if we are running in Gold or Silver mode
-* @param account The account to print
-*/
 void printAccountInfo(const Account &account) {
     format_on();
     print("Account Information:\n");
     format_on();
-    cout << "Account Holder: " << account.name << "\n";
-    cout << "Account Type: " << ((account.balance >= 1000000) ? "GOLD" : "SILVER") << "\n";
-    cout << "Account Balance: $" << account.balance << "\n\n";
+    cout << "Account Holder: ";
+    format_out_on();
+    cout<< account.name << "\n";
+    format_on();
+    cout << "Account Type: ";
+    format_out_on();
+    cout << ((account.balance >= 1000000) ? "GOLD" : "SILVER") << "\n";
+    void format_on();
+    print( "Account Balance: $") ;
+    format_out_on();
+    cout<< account.balance << "\n\n";
     format_off();
 }
 
 // Print formatted text
-/**
-* @brief Prints a string to the screen. This is a debugging function to be used in conjunction with print_string
-* @param text The string to print
-*/
 void print(string text) {
     cout << "\033[1m\033[32;3m" << text;
     cout << "\033[0m";
 }
-
+void print_error(string text) {
+    cout << "\033[1m\033[31;3m" << text;
+    cout << "\033[0m";
+}
 // Enable text formatting
-/**
-* @brief \ fn format_on \ brief Turn on formating \ param [ in ] none \ param [ out ]
-*/
 void format_on() {
     cout << "\033[1m\033[32;3m";
 }
-
+void format_out_on(){
+        cout << "\033[1m\033[34;3m";
+}
 // Disable text formatting
-/**
-* @brief \ brief Turn off formatting \ ingroup QGIS_QUICK_Eastern_
-*/
 void format_off() {
     cout << "\033[0m";
 }
+
 // Check passcode for correctness and convert to integer
-/**
-* @brief Check passcode for validity and return number of passcodes. Print error if invalid passcode. Return value is 0 for success or non - zero for failure.
-* @return number of passcodes in string or - 1 for failure ( check_passcode () is called by this
-*/
 int check_passcode() {
     string passcode;
     cin >> passcode;
 
-    // check_passcode Checks if passcode is exactly four digits long
     if (passcode.length() != 4) {
         print(
             "Passcode must be exactly four digits long.\n"
@@ -334,7 +309,6 @@ int check_passcode() {
     }
 
     for (char c : passcode) {
-        // check if passcode is four digit number
         if (!isdigit(c)) {
             print("Passcode must be a four-digit number.\n");
             return check_passcode();
@@ -345,27 +319,17 @@ int check_passcode() {
 }
 
 // Check if user input is between min and max
-/**
-* @brief Checks if user input is between min and max. If not prints error to user and try again.
-* @param min Minimum value to check. This is the minimum allowed value.
-* @param max Maximum value to check. This is the maximum allowed value.
-* @return Returns the input value or - 1 if user enters invalid input ( in which case error is printed
-*/
 int check_int_input(int min, int max) {
     int input;
     cin >> input;
 
-    // Check if the input is valid.
     if (cin.fail()) {
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        print("Invalid input. Please enter a valid number: ");
+        print_error("Invalid input. Please enter a valid number: ");
         return check_int_input(min, max);
-    // Check if the input is within the range min max.
     } else if (input < min || input > max) {
-        format_on();
-        cout << "Invalid input. Please try again:";
-        format_off();
+        print_error("Invalid input. Please try again:");
         return check_int_input(min, max);
     } else {
         return input;
